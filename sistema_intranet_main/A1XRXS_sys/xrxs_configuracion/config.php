36aad6c027804ae9b2cb1c3a0ab4d7174280c40e
Victor Reyes Galvez <tenshi98@gmail.com>
2021-08-02 19:03:40 -0400

    Eliminacion datos
    Se eliminan los datos de acceso al servidor

--- sistema_intranet_main/A1XRXS_sys/xrxs_configuracion/config.php
+++ sistema_intranet_main/A1XRXS_sys/xrxs_configuracion/config.php
@@ -29,8 +29,8 @@ if( in_array( $_SERVER['REMOTE_ADDR'], $whitelist) ){
 	define( 'DB_SITE_MAIN', 'http://localhost/power_engine/sistema_intranet_main' );           //URL del sistema
 	define( 'DB_SITE_MAIN_PATH', '/sistema_intranet_main');                                    //Path de la carpeta contenedora
 	//sitios externos
-	define( 'DB_SITE_ALT_1', 'http://localhost/power_engine/sistema_web_clientes_crosstech' ); //URL 1 para uso interno
-	define( 'DB_SITE_ALT_1_PATH', '/sistema_web_clientes_crosstech');                          //Path de la carpeta contenedora
+	define( 'DB_SITE_ALT_1', 'http://localhost/power_engine/sistema_web_clientes' );           //URL 1 para uso interno
+	define( 'DB_SITE_ALT_1_PATH', '/sistema_web_clientes');                                    //Path de la carpeta contenedora
 	define( 'DB_SITE_ALT_2', '' );                                                             //URL 2 para uso interno
 	define( 'DB_SITE_ALT_2_PATH', '');                                                         //Path de la carpeta contenedora
 	define( 'DB_SITE_ALT_3', '' );                                                             //URL 3 para uso interno
@@ -43,13 +43,13 @@ if( in_array( $_SERVER['REMOTE_ADDR'], $whitelist) ){
 	
 	/*******************************************/
 	//Empresa
-	define( 'DB_EMPRESA_NAME', 'Crosstech');
-	define( 'DB_EMPRESA_MAIL', 'notificaciones@crosstech.cl');
+	define( 'DB_EMPRESA_NAME', 'ASD');
+	define( 'DB_EMPRESA_MAIL', 'zzxc@zxc.cl');
 	
 	/*******************************************/
 	//Debug
-	define( 'DB_ERROR_MAIL', 'vreyes@crosstech.cl');
-	define( 'DB_COMPARE', 'crosstech_power_engine_main');
+	define( 'DB_ERROR_MAIL', 'zxc@zxc.cl');
+	define( 'DB_COMPARE', 'power_engine_main');
 	
 	/*******************************************/
 	//Notificaciones
@@ -63,19 +63,19 @@ if( in_array( $_SERVER['REMOTE_ADDR'], $whitelist) ){
 	/*******************************************/
 	//Servidor
 	define( 'DB_SERVER', 'localhost' );
-	define( 'DB_NAME', 'crosstech_pe_intranet');
-	define( 'DB_USER', 'crosstech_admin');
-	define( 'DB_PASS', '&-VSda,#rFvT');
+	define( 'DB_NAME', 'intranet');
+	define( 'DB_USER', 'admin');
+	define( 'DB_PASS', 'zxcasdqwe');
 	
 	/*******************************************/
 	//Repositorio
-	define( 'DB_SITE_REPO', 'https://repositorio.crosstech.cl' );       //repositorio
+	define( 'DB_SITE_REPO', 'https://repositorio.example.cl' );         //repositorio
 	//Sitio
-	define( 'DB_SITE_MAIN', 'https://intranet.crosstech.cl' );          //URL del sistema
-	define( 'DB_SITE_MAIN_PATH', '/sistema_intranet_intranet');         //Path de la carpeta contenedora
+	define( 'DB_SITE_MAIN', 'https://intranet.example.cl' );            //URL del sistema
+	define( 'DB_SITE_MAIN_PATH', '/sistema_intranet');                  //Path de la carpeta contenedora
 	//sitios externos
-	define( 'DB_SITE_ALT_1', 'https://plataforma.crosstech.cl' );       //URL 1 para uso interno
-	define( 'DB_SITE_ALT_1_PATH', '/sistema_web_clientes_crosstech');   //Path de la carpeta contenedora
+	define( 'DB_SITE_ALT_1', 'https://plataforma.example.cl' );         //URL 1 para uso interno
+	define( 'DB_SITE_ALT_1_PATH', '/sistema_web_clientes');             //Path de la carpeta contenedora
 	define( 'DB_SITE_ALT_2', '' );                                      //URL 2 para uso interno
 	define( 'DB_SITE_ALT_2_PATH', '');                                  //Path de la carpeta contenedora
 	define( 'DB_SITE_ALT_3', '' );                                      //URL 3 para uso interno
@@ -88,13 +88,13 @@ if( in_array( $_SERVER['REMOTE_ADDR'], $whitelist) ){
 	
 	/*******************************************/
 	//Empresa
-	define( 'DB_EMPRESA_NAME', 'Crosstech');
-	define( 'DB_EMPRESA_MAIL', 'notificaciones@crosstech.cl');
+	define( 'DB_EMPRESA_NAME', 'ASD');
+	define( 'DB_EMPRESA_MAIL', 'zzxc@zxc.cl');
 	
 	/*******************************************/
 	//Debug
-	define( 'DB_ERROR_MAIL', 'vreyes@crosstech.cl');
-	define( 'DB_COMPARE', 'crosstech_power_engine_main');
+	define( 'DB_ERROR_MAIL', 'zxc@zxc.cl');
+	define( 'DB_COMPARE', 'power_engine_main');
 	
 	/*******************************************/
 	//Notificaciones
