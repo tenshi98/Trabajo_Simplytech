50da3f296747794525fc9b6a0d0174add6fda793
tenshi98 <54037366+tenshi98@users.noreply.github.com>
2021-08-20 17:57:43 -0400

    Se agregan nuevas dependencias
    Se agregan la dependencia a todos los nuevos componentes

--- sistema_intranet_main/core/Web.Footer.Main.php
+++ sistema_intranet_main/core/Web.Footer.Main.php
@@ -1,48 +1,50 @@
-			</div>
+					</div>
+				</div>
+			</div> 
 		</div>
-	</div> 
-</div>
-<footer id="footer">
-	<p><?php echo ano_actual();?> &copy; <?php echo DB_EMPRESA_NAME ?> Todos los derechos reservados.</p>
-</footer>
+		<footer id="footer">
+			<p><?php echo ano_actual();?> &copy; <?php echo DB_EMPRESA_NAME ?> Todos los derechos reservados.</p>
+		</footer>
 
-<!-- Animacion carga pagina -->
-<script>
-	//ocultar el loader
-	$(document).ready(function() {
-		setTimeout(function(){
-			$('body').addClass('loaded');
-		}, 500);
-	});
-	//ajustar tamaño de todos los textarea
-	autosize(document.querySelectorAll('textarea'));
-</script>
+		<!-- Animacion carga pagina -->
+		<script>
+			//ocultar el loader
+			$(document).ready(function() {
+				setTimeout(function(){
+					$('body').addClass('loaded');
+				}, 500);
+			});
+			//ajustar tamaño de todos los textarea
+			autosize(document.querySelectorAll('textarea'));
+			//se toma raiz
+			var domain_val = "<?php echo DB_SITE_REPO ?>";
+		</script>
 
-<!--Otros archivos javascript -->
-<script type="text/javascript" src="<?php echo DB_SITE_REPO ?>/LIB_assets/lib/bootstrap3/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="<?php echo DB_SITE_REPO ?>/LIB_assets/lib/screenfull/screenfull.js"></script> 
-<script type="text/javascript" src="<?php echo DB_SITE_REPO ?>/LIB_assets/js/jquery-ui-1.10.3.min.js"></script>
-<script type="text/javascript" src="<?php echo DB_SITE_REPO ?>/LIB_assets/js/main.min.js"></script>
-<script type="text/javascript" src="<?php echo DB_SITE_REPO ?>/LIBS_js/prism/prism.js"></script>
-
-<?php
-/******************************************************************************************************/
-/*                              Ventana Debug Superadministrador                                      */
-/******************************************************************************************************/
-require_once '../A2XRXS_gears/xrxs_funciones/Helpers.Utils.Result.php'; 
-/******************************************************************************************************/
-//cuadro mensajes
-widget_avgrund();
-/******************************************************************************************************/
-/*                                                                                                    */
-/*                                              CIERRE DE LA BASE                                     */
-/*                                                                                                    */
-/******************************************************************************************************/
-mysqli_close($dbConn);
-
-?>
+		<!--Otros archivos javascript -->
+		<script type="text/javascript" src="<?php echo DB_SITE_REPO ?>/LIB_assets/lib/bootstrap3/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="<?php echo DB_SITE_REPO ?>/LIB_assets/lib/screenfull/screenfull.js"></script> 
+		<script type="text/javascript" src="<?php echo DB_SITE_REPO ?>/LIB_assets/js/jquery-ui-1.10.3.min.js"></script>
+		<script type="text/javascript" src="<?php echo DB_SITE_REPO ?>/LIB_assets/js/main.min.js"></script>
+		<script type="text/javascript" src="<?php echo DB_SITE_REPO ?>/LIBS_js/prism/prism.js"></script>
+		<script type="text/javascript" src="<?php echo DB_SITE_REPO ?>/LIBS_js/country_picker/js/countrypicker.js"></script>
 		
-		<!-- Librerias -->
-		<?php widget_tooltipster();?>
+		<?php
+		/******************************************************************************************************/
+		/*                              Ventana Debug Superadministrador                                      */
+		/******************************************************************************************************/
+		require_once '../A2XRXS_gears/xrxs_funciones/Helpers.Utils.Result.php'; 
+		/******************************************************************************************************/
+		//cuadro mensajes
+		widget_avgrund();
+		//tooltip
+		widget_tooltipster();
+		/******************************************************************************************************/
+		/*                                                                                                    */
+		/*                                              CIERRE DE LA BASE                                     */
+		/*                                                                                                    */
+		/******************************************************************************************************/
+		mysqli_close($dbConn);
+		
+		?>
 	</body>
 </html>
