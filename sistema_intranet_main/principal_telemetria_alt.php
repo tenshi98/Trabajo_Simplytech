e0a47c002f8b44cbe8baaa75386a0fcbc1676601
tenshi98 <54037366+tenshi98@users.noreply.github.com>
2021-08-24 13:05:38 -0400

    Actualizacion dependencias
    Actualizacion dependencias, se eliminan dependencias antiguas y se agregan las nuevas, se agrega excepcion en la conexion mysql

--- sistema_intranet_main/principal_telemetria_alt.php
+++ sistema_intranet_main/principal_telemetria_alt.php
@@ -6,34 +6,11 @@ define('XMBCXRXSKGC', 1);
 /**********************************************************************************************************************************/
 /*                                                     Ejecucion codigo                                                           */
 /**********************************************************************************************************************************/
-//Configuracion de la plataforma
-require_once 'A1XRXS_sys/xrxs_configuracion/config.php';
+require_once 'A1XRXS_sys/xrxs_configuracion/config.php';                                  //Configuracion de la plataforma
+require_once '../Legacy/gestion_modular/funciones/Helpers.Functions.Propias.php';         //carga librerias de la plataforma
+require_once '../Legacy/gestion_modular/funciones/Components.UI.Widgets.Extended.php';    //carga widgets de la plataforma
+require_once '../A2XRXS_gears/xrxs_configuracion/Load.User.Session.php';                  //verificacion sesion usuario
 
-//Carga de las funciones del nucleo
-require_once '../A2XRXS_gears/xrxs_funciones/Helpers.Utils.Load.php';                  //Carga de variables
-require_once '../A2XRXS_gears/xrxs_funciones/Helpers.Functions.Common.php';            //Funciones comunes
-require_once '../A2XRXS_gears/xrxs_funciones/Helpers.Functions.Convertions.php';       //Conversiones de datos
-require_once '../A2XRXS_gears/xrxs_funciones/Helpers.Functions.Data.Date.php';         //Funciones relacionadas a las fechas
-require_once '../A2XRXS_gears/xrxs_funciones/Helpers.Functions.Data.Numbers.php';      //Funciones relacionadas a los numeros
-require_once '../A2XRXS_gears/xrxs_funciones/Helpers.Functions.Data.Operations.php';   //Funciones relacionadas a operaciones matematicas
-require_once '../A2XRXS_gears/xrxs_funciones/Helpers.Functions.Data.Text.php';         //Funciones relacionadas a los textos
-require_once '../A2XRXS_gears/xrxs_funciones/Helpers.Functions.Data.Time.php';         //Funciones relacionadas a las horas
-require_once '../A2XRXS_gears/xrxs_funciones/Helpers.Functions.Data.Validations.php';  //Funciones de validacion de datos
-require_once '../A2XRXS_gears/xrxs_funciones/Helpers.Functions.DataBase.php';          //Funciones relacionadas a la base de datos
-require_once '../A2XRXS_gears/xrxs_funciones/Helpers.Functions.Location.php';          //Funciones relacionadas a la geolozalizacion
-require_once '../A2XRXS_gears/xrxs_funciones/Helpers.Functions.Server.Client.php';     //Funciones para entregar informacion del cliente
-require_once '../A2XRXS_gears/xrxs_funciones/Helpers.Functions.Server.Server.php';     //Funciones para entregar informacion del servidor
-require_once '../A2XRXS_gears/xrxs_funciones/Helpers.Functions.Server.Web.php';        //Funciones para entregar informacion de la web
-
-//Carga de los componentes de los formularios
-require_once '../A2XRXS_gears/xrxs_funciones/Components.UI.Widgets.php';
-
-//verificacion sesion usuario
-require_once '../A2XRXS_gears/xrxs_configuracion/Load.User.Session.php';
-
-//carga librerias propias de la plataforma
-require_once '../Legacy/gestion_modular/funciones/Helpers.Functions.Propias.php';
-require_once '../Legacy/gestion_modular/funciones/Components.UI.Widgets.Extended.php';
 
 
 echo '<span class="panel-title"  style="color: #1E90FF;font-weight: 700 !important;">Hora Refresco: '.hora_actual().'</span>';
