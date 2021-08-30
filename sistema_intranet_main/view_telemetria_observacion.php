20fd4a7ef91b5794644d8624191fe0bbb318d29a
tenshi98 <54037366+tenshi98@users.noreply.github.com>
2021-08-30 18:12:05 -0400

    Creacion view observaciones
    Creacion view observaciones de los equipos de telemetria

---
+++ sistema_intranet_main/view_telemetria_observacion.php
@@ -0,0 +1,108 @@
+<?php session_start();
+/**********************************************************************************************************************************/
+/*                                           Se define la variable de seguridad                                                   */
+/**********************************************************************************************************************************/
+define('XMBCXRXSKGC', 1);
+/**********************************************************************************************************************************/
+/*                                          Se llaman a los archivos necesarios                                                   */
+/**********************************************************************************************************************************/
+require_once 'core/Load.Utils.Views.php';
+/**********************************************************************************************************************************/
+/*                                                 Variables Globales                                                             */
+/**********************************************************************************************************************************/
+//Tiempo Maximo de la consulta, 40 minutos por defecto
+if(isset($_SESSION['usuario']['basic_data']['ConfigTime'])&&$_SESSION['usuario']['basic_data']['ConfigTime']!=0){$n_lim = $_SESSION['usuario']['basic_data']['ConfigTime']*60;set_time_limit($n_lim); }else{set_time_limit(2400);}             
+//Memora RAM Maxima del servidor, 4GB por defecto
+if(isset($_SESSION['usuario']['basic_data']['ConfigRam'])&&$_SESSION['usuario']['basic_data']['ConfigRam']!=0){$n_ram = $_SESSION['usuario']['basic_data']['ConfigRam']; ini_set('memory_limit', $n_ram.'M'); }else{ini_set('memory_limit', '4096M');}  
+/**********************************************************************************************************************************/
+/*                                         Se llaman a la cabecera del documento html                                             */
+/**********************************************************************************************************************************/
+require_once 'core/Web.Header.Views.php';
+/**********************************************************************************************************************************/
+/*                                                   ejecucion de logica                                                          */
+/**********************************************************************************************************************************/
+//Version antigua de view
+//se verifica si es un numero lo que se recibe
+if (validarNumero($_GET['view'])){ 
+	//Verifica si el numero recibido es un entero
+	if (validaEntero($_GET['view'])){ 
+		$X_Puntero = $_GET['view'];
+	} else { 
+		$X_Puntero = simpleDecode($_GET['view'], fecha_actual());
+	}
+} else { 
+	$X_Puntero = simpleDecode($_GET['view'], fecha_actual());
+}
+/**************************************************************/
+// consulto los datos
+$SIS_query = '
+telemetria_listado.Nombre AS nombre_equipo,
+usuarios_listado.Nombre AS nombre_usuario,
+telemetria_listado_observaciones.Fecha,
+telemetria_listado_observaciones.Observacion';
+$SIS_join  = '
+LEFT JOIN `telemetria_listado` ON telemetria_listado.idTelemetria     = telemetria_listado_observaciones.idTelemetria
+LEFT JOIN `usuarios_listado`   ON usuarios_listado.idUsuario          = telemetria_listado_observaciones.idUsuario';
+$SIS_where = 'telemetria_listado_observaciones.idObservacion ='.$X_Puntero;
+$rowdata = db_select_data (false, $SIS_query, 'telemetria_listado_observaciones', $SIS_join, $SIS_where, $dbConn, $_SESSION['usuario']['basic_data']['Nombre'], basename($_SERVER["REQUEST_URI"], ".php"), 'rowdata');
+
+
+?>
+<div class="col-sm-12">
+	<div class="box">	
+		<header>		
+			<h5>Ver Datos de la Observacion</h5>		
+		</header>
+        <div class="body">
+            <h2 class="text-primary">Datos Basicos</h2>
+            <p class="text-muted">
+				<strong>Equipo : </strong><?php echo $rowdata['nombre_equipo']; ?><br/>
+				<strong>Usuario : </strong><?php echo $rowdata['nombre_usuario']; ?><br/>
+				<strong>Fecha : </strong><?php echo Fecha_completa_alt($rowdata['Fecha']); ?>
+            </p>
+                      
+            <h2 class="text-primary">Observacion</h2>
+            <p class="text-muted word_break">
+				<div class="text-muted well well-sm no-shadow">
+					<?php if(isset($rowdata['Observacion'])&&$rowdata['Observacion']!=''){echo $rowdata['Observacion'];}else{echo 'Sin Observaciones';} ?>
+					<div class="clearfix"></div>
+				</div>
+			</p>
+            
+        	
+        </div>
+	</div>
+</div>
+<?php 
+//si se entrega la opcion de mostrar boton volver
+if(isset($_GET['return'])&&$_GET['return']!=''){ 
+	//para las versiones antiguas
+	if($_GET['return']=='true'){ ?>
+		<div class="clearfix"></div>
+		<div class="col-sm-12" style="margin-bottom:30px;margin-top:30px;">
+			<a href="#" onclick="history.back()" class="btn btn-danger fright"><i class="fa fa-arrow-left" aria-hidden="true"></i> Volver</a>
+			<div class="clearfix"></div>
+		</div>
+	<?php 
+	//para las versiones nuevas que indican donde volver
+	}else{ 
+		$string = basename($_SERVER["REQUEST_URI"], ".php");
+		$array  = explode("&return=", $string, 3);
+		$volver = $array[1];
+		?>
+		<div class="clearfix"></div>
+		<div class="col-sm-12" style="margin-bottom:30px;margin-top:30px;">
+			<a href="<?php echo $volver; ?>" class="btn btn-danger fright"><i class="fa fa-arrow-left" aria-hidden="true"></i> Volver</a>
+			<div class="clearfix"></div>
+		</div>
+		
+	<?php }		
+} ?>
+
+
+<?php
+/**********************************************************************************************************************************/
+/*                                             Se llama al pie del documento html                                                 */
+/**********************************************************************************************************************************/
+require_once 'core/Web.Footer.Views.php';
+?>
