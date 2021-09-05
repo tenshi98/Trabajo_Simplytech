d9c0a960ac833cc6d73b673f9e669748c4b9d19f
tenshi98 <54037366+tenshi98@users.noreply.github.com>
2021-09-05 14:13:13 -0400

    Se agregan dependencias
    Se agregan dependencias de las ventanas popup y los cuadros emergentes

--- sistema_intranet_main/core/Web.Footer.Error.php
+++ sistema_intranet_main/core/Web.Footer.Error.php
@@ -1,5 +1,6 @@
 		<!--Otros archivos javascript -->
 		<script type="text/javascript" src="<?php echo DB_SITE_REPO ?>/LIBS_js/prism/prism.js"></script>
+		<script type="text/javascript" src="<?php echo DB_SITE_REPO ?>/LIBS_js/tooltipster/js/tooltipster.bundle.min.js"></script>
 		<!-- Animacion carga pagina -->
 		<script>
 			//ocultar el loader
@@ -7,6 +8,13 @@
 				setTimeout(function(){
 					$('body').addClass('loaded');
 				}, 1000);
+				
+				//Burbuja de ayuda
+				$('.tooltip').tooltipster({
+					animation: 'grow',
+					delay: 130,
+					maxWidth: 300
+				});
 			});
 			//ajustar tamaño de todos los textarea
 			autosize(document.querySelectorAll('textarea'));
