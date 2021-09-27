c6e2a8bd3cd5cada79054d75b66ff5ae8831a6f1
tenshi98 <54037366+tenshi98@users.noreply.github.com>
2021-09-27 16:22:21 -0400

    Nueva transaccion
    Nueva transaccion

---
+++ sistema_intranet_main/telemetria_data_files.php
@@ -0,0 +1,261 @@
+<?php session_start();
+/**********************************************************************************************************************************/
+/*                                           Se define la variable de seguridad                                                   */
+/**********************************************************************************************************************************/
+define('XMBCXRXSKGC', 1);
+/**********************************************************************************************************************************/
+/*                                          Se llaman a los archivos necesarios                                                   */
+/**********************************************************************************************************************************/
+require_once 'core/Load.Utils.Web.php';
+/**********************************************************************************************************************************/
+/*                                          Modulo de identificacion del documento                                                */
+/**********************************************************************************************************************************/
+//Cargamos la ubicacion 
+$original = "telemetria_data_files.php";
+$location = $original;
+//Se agregan ubicaciones
+$location .='?pagina='.$_GET['pagina'];
+/********************************************************************/
+//Variables para filtro y paginacion
+$search = '';
+if(isset($_GET['Nombre']) && $_GET['Nombre'] != ''){      $location .= "&Nombre=".$_GET['Nombre'];      $search .= "&Nombre=".$_GET['Nombre'];}
+/********************************************************************/
+//Verifico los permisos del usuario sobre la transaccion
+require_once '../A2XRXS_gears/xrxs_configuracion/Load.User.Permission.php';
+/**********************************************************************************************************************************/
+/*                                         Se llaman a la cabecera del documento html                                             */
+/**********************************************************************************************************************************/
+require_once 'core/Web.Header.Main.php';
+/**********************************************************************************************************************************/
+/*                                                   ejecucion de logica                                                          */
+/**********************************************************************************************************************************/
+if ( ! empty($_GET['id']) ) { 
+//valido los permisos
+validaPermisoUser($rowlevel['level'], 1, $dbConn);
+/********************************************/
+//se consulta
+$SIS_query = 'Nombre,FTP_Carpeta';
+$SIS_join  = '';
+$SIS_where = 'idTelemetria = '.$_GET['id'];
+$rowdata = db_select_data (false, $SIS_query, 'telemetria_listado', $SIS_join, $SIS_where, $dbConn, $_SESSION['usuario']['basic_data']['Nombre'], $original, 'rowdata');
+
+//muestro enlace
+alert_post_data(2,1,1, 'Se recomienda utilizar el reproductor VLC Player, se puede descargar desde <a target="_blank" rel="noopener noreferrer" href="https://www.videolan.org/vlc/index.es.html">Aqui</a>');
+
+//verifico que exista el rut
+if(isset($rowdata["FTP_Carpeta"])&&$rowdata["FTP_Carpeta"]!=''){
+	//se crea la ruta
+	$ruta     = DB_SITE_MAIN.'/ClientFiles/index.php?FTPFolder='.$rowdata["FTP_Carpeta"];
+	$s_folder = DB_SITE_MAIN.'/ClientFiles/_data/'.$rowdata["FTP_Carpeta"];
+		
+	//si no existe la carpeta, la creo
+	if (!file_exists($s_folder)) {
+		try {
+			$oldmask = umask(000);//it will set the new umask and returns the old one 
+			mkdir($s_folder, 0777);
+			umask($oldmask);//reset the old umask
+			//muestro la carpeta
+			echo '
+			<style>
+				.iframe_elfinder{height: 1500px;}
+				iframe{float:right;width: 100%;height: 100%;padding: 0;margin: 0;border:none;}
+			</style>
+			<div class="iframe_elfinder">
+				<iframe class="embed-responsive-item" src="'.$ruta.'" allowfullscreen></iframe>
+			</div>';
+					
+		} catch (Exception $e) {
+			alert_post_data(4,1,1, 'Error '.$e->getMessage());
+		}
+	//si existe carpeta muestro	
+	}else{
+		//muestro la carpeta
+		echo '
+		<style>
+			.iframe_elfinder{height: 1500px;}
+			iframe{float:right;width: 100%;height: 100%;padding: 0;margin: 0;border:none;}
+		</style>
+		<div class="iframe_elfinder">
+			<iframe class="embed-responsive-item" src="'.$ruta.'" allowfullscreen></iframe>
+		</div>';
+	}
+//si no existe la carpeta
+}else{
+	alert_post_data(4,1,1, 'No existe la carpeta contenedora');
+}
+	
+	
+?>	
+
+	
+<div class="clearfix"></div>
+<div class="col-sm-12" style="margin-bottom:30px">
+<a href="<?php echo $location ?>" class="btn btn-danger fright"><i class="fa fa-arrow-left" aria-hidden="true"></i> Volver</a>
+<div class="clearfix"></div>
+</div>
+ 
+<?php ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+ } else  { 
+/**********************************************************/
+//paginador de resultados
+if(isset($_GET["pagina"])){
+	$num_pag = $_GET["pagina"];	
+} else {
+	$num_pag = 1;	
+}
+//Defino la cantidad total de elementos por pagina
+$cant_reg = 30;
+//resto de variables
+if (!$num_pag){
+	$comienzo = 0 ;
+	$num_pag = 1 ;
+} else {
+	$comienzo = ( $num_pag - 1 ) * $cant_reg ;
+}
+/**********************************************************/
+//ordenamiento
+if(isset($_GET['order_by'])&&$_GET['order_by']!=''){
+	switch ($_GET['order_by']) {
+		case 'nombre_asc':     $order_by = 'telemetria_listado.Nombre ASC ';          $bread_order = '<i class="fa fa-sort-alpha-asc" aria-hidden="true"></i> Nombre Ascendente'; break;
+		case 'nombre_desc':    $order_by = 'telemetria_listado.Nombre DESC ';         $bread_order = '<i class="fa fa-sort-alpha-desc" aria-hidden="true"></i> Nombre Descendente';break;
+		case 'carpeta_asc':    $order_by = 'telemetria_listado.FTP_Carpeta ASC ';     $bread_order = '<i class="fa fa-sort-alpha-asc" aria-hidden="true"></i> Carpeta FTP Ascendente'; break;
+		case 'carpeta_desc':   $order_by = 'telemetria_listado.FTP_Carpeta DESC ';    $bread_order = '<i class="fa fa-sort-alpha-desc" aria-hidden="true"></i> Carpeta FTP Descendente';break;
+		
+		default: $order_by = 'telemetria_listado.Nombre ASC '; $bread_order = '<i class="fa fa-sort-alpha-asc" aria-hidden="true"></i> Nombre Ascendente';
+	}
+}else{
+	$order_by = 'telemetria_listado.Nombre ASC '; $bread_order = '<i class="fa fa-sort-alpha-asc" aria-hidden="true"></i> Nombre Ascendente';
+}
+/**********************************************************/
+//Variable de busqueda
+$SIS_where  = "telemetria_listado.idEstado = 1"; //Activo
+$SIS_where .= " AND telemetria_listado.idUsoFTP = 1"; //Utiliza FTP
+$SIS_where .= " AND telemetria_listado.idSistema=".$_SESSION['usuario']['basic_data']['idSistema'];
+//Verifico el tipo de usuario que esta ingresando y el id
+$SIS_join = "";	
+if(isset($_SESSION['usuario']['basic_data']['idTipoUsuario'])&&$_SESSION['usuario']['basic_data']['idTipoUsuario']!=1){
+	$SIS_join .= " INNER JOIN usuarios_equipos_telemetria ON usuarios_equipos_telemetria.idTelemetria = telemetria_listado.idTelemetria ";	
+	$SIS_where .= " AND usuarios_equipos_telemetria.idUsuario = ".$_SESSION['usuario']['basic_data']['idUsuario'];	
+}
+/**********************************************************/
+//Se aplican los filtros
+if(isset($_GET['Nombre']) && $_GET['Nombre'] != ''){           $SIS_where .= " AND telemetria_listado.Nombre LIKE '%".$_GET['Nombre']."%'";}
+if(isset($_GET['FTP_Carpeta']) && $_GET['FTP_Carpeta'] != ''){ $SIS_where .= " AND telemetria_listado.FTP_Carpeta LIKE '%".$_GET['FTP_Carpeta']."%'";}
+/**********************************************************/
+//Realizo una consulta para saber el total de elementos existentes
+$cuenta_registros = db_select_nrows (false, 'telemetria_listado.idTelemetria', 'telemetria_listado', $SIS_join, $SIS_where, $dbConn, $_SESSION['usuario']['basic_data']['Nombre'], $original, 'cuenta_registros');
+//Realizo la operacion para saber la cantidad de paginas que hay
+$total_paginas = ceil($cuenta_registros / $cant_reg);
+// Se trae un listado con todos los elementos
+$SIS_query = '
+telemetria_listado.idTelemetria, 
+telemetria_listado.Nombre, 
+telemetria_listado.FTP_Carpeta';
+$SIS_order = $order_by.' LIMIT '.$comienzo.', '.$cant_reg;
+$arrEquipo = array();
+$arrEquipo = db_select_array (false, $SIS_query, 'telemetria_listado', $SIS_join, $SIS_where, $SIS_order, $dbConn, $_SESSION['usuario']['basic_data']['Nombre'], $original, 'arrEquipos');
+
+?>
+
+<div class="col-sm-12 breadcrumb-bar">
+
+	<ul class="btn-group btn-breadcrumb pull-left">
+		<li class="btn btn-default tooltip" role="button" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample" title="Presionar para desplegar Formulario de Busqueda" style="font-size: 14px;"><i class="fa fa-search faa-vertical animated" aria-hidden="true"></i></li>
+		<li class="btn btn-default"><?php echo $bread_order; ?></li>
+		<?php if(isset($_GET['filtro_form'])&&$_GET['filtro_form']!=''){ ?>
+			<li class="btn btn-danger"><a href="<?php echo $original.'?pagina=1'; ?>" style="color:#fff;"><i class="fa fa-trash-o" aria-hidden="true"></i> Limpiar</a></li>
+		<?php } ?>		
+	</ul>
+	
+</div>
+<div class="clearfix"></div> 
+<div class="collapse col-sm-12" id="collapseExample">
+	<div class="well">
+		<div class="col-sm-8 fcenter">
+			<form class="form-horizontal" id="form1" name="form1" action="<?php echo $location; ?>" novalidate>
+				<?php 
+				//Se verifican si existen los datos
+				if(isset($Nombre)) {       $x1 = $Nombre;       }else{$x1 = '';}
+				if(isset($FTP_Carpeta)) {  $x2 = $FTP_Carpeta;  }else{$x2 = '';}
+
+				//se dibujan los inputs
+				$Form_Inputs = new Form_Inputs();
+				$Form_Inputs->form_input_text('Equipo', 'Nombre', $x1, 1);
+				$Form_Inputs->form_input_text('Carpeta FTP', 'FTP_Carpeta', $x2, 1);
+				
+				$Form_Inputs->form_input_hidden('pagina', $_GET['pagina'], 1);
+				?>
+				
+				<div class="form-group">
+					<input type="submit" class="btn btn-primary fright margin_width fa-input" value="&#xf002; Filtrar" name="filtro_form">
+					<a href="<?php echo $original.'?pagina=1'; ?>" class="btn btn-danger fright margin_width"><i class="fa fa-trash-o" aria-hidden="true"></i> Limpiar</a>
+				</div>
+                      
+			</form> 
+            <?php widget_validator(); ?>
+        </div>
+	</div>
+</div>
+<div class="clearfix"></div> 
+                      
+                                 
+<div class="col-sm-12">
+	<div class="box">
+		<header>
+			<div class="icons"><i class="fa fa-table" aria-hidden="true"></i></div><h5>Listado de Categorias</h5>
+			<div class="toolbar">
+				<?php 
+				//se llama al paginador
+				echo paginador_2('pagsup',$total_paginas, $original, $search, $num_pag ) ?>
+			</div>
+		</header>
+		<div class="table-responsive">
+			<table id="dataTable" class="table table-bordered table-condensed table-hover table-striped dataTable">
+				<thead>
+					<tr role="row">
+						<th>
+							<div class="pull-left">Nombre</div>
+							<div class="btn-group pull-right" style="width: 50px;" >
+								<a href="<?php echo $location.'&order_by=nombre_asc'; ?>" title="Ascendente" class="btn btn-default btn-xs tooltip"><i class="fa fa-sort-alpha-asc" aria-hidden="true"></i></a>
+								<a href="<?php echo $location.'&order_by=nombre_desc'; ?>" title="Descendente" class="btn btn-default btn-xs tooltip"><i class="fa fa-sort-alpha-desc" aria-hidden="true"></i></a>
+							</div>
+						</th>
+						<th>
+							<div class="pull-left">Carpeta FTP</div>
+							<div class="btn-group pull-right" style="width: 50px;" >
+								<a href="<?php echo $location.'&order_by=carpeta_asc'; ?>" title="Ascendente" class="btn btn-default btn-xs tooltip"><i class="fa fa-sort-alpha-asc" aria-hidden="true"></i></a>
+								<a href="<?php echo $location.'&order_by=carpeta_desc'; ?>" title="Descendente" class="btn btn-default btn-xs tooltip"><i class="fa fa-sort-alpha-desc" aria-hidden="true"></i></a>
+							</div>
+						</th>
+						<th width="10">Acciones</th>
+					</tr>
+				</thead>				  
+				<tbody role="alert" aria-live="polite" aria-relevant="all">
+				<?php foreach ($arrEquipo as $equipo) { ?>
+					<tr class="odd">
+						<td><?php echo $equipo['Nombre']; ?></td>
+						<td><?php echo $equipo['FTP_Carpeta']; ?></td>
+						<td>
+							<div class="btn-group" style="width: 35px;" >
+								<?php if ($rowlevel['level']>=1){?><a href="<?php echo $location.'&id='.$equipo['idTelemetria']; ?>" title="Ver Datos" class="btn btn-primary btn-sm tooltip"><i class="fa fa-list" aria-hidden="true"></i></a><?php } ?>
+							</div>
+						</td>
+					</tr>
+				<?php } ?>                    
+				</tbody>
+			</table>
+		</div>
+		<div class="pagrow">	
+			<?php 
+			//se llama al paginador
+			echo paginador_2('paginf',$total_paginas, $original, $search, $num_pag ) ?>
+		</div>
+	</div>
+</div>
+<?php } ?>           
+<?php
+/**********************************************************************************************************************************/
+/*                                             Se llama al pie del documento html                                                 */
+/**********************************************************************************************************************************/
+require_once 'core/Web.Footer.Main.php';
+?>
