51b10b29d1d22e321a24f01926ec6925d50a8f08
tenshi98 <54037366+tenshi98@users.noreply.github.com>
2021-10-01 17:45:37 -0400

    Se agrega respaldo de datos
    Se agrega respaldo de datos por parte del atacante

--- sistema_intranet_main/403.php
+++ sistema_intranet_main/403.php
@@ -10,7 +10,25 @@ require_once 'core/Load.Utils.Error.php';
 /**********************************************************************************************************************************/
 /*                                         Se llaman a la cabecera del documento html                                             */
 /**********************************************************************************************************************************/
+header('HTTP/1.1 403 Forbidden');
 require_once 'core/Web.Header.Error.php';
+
+error_reporting(0); 
+$post_query = '';
+if(isset($_POST)){foreach($_POST as $key=>$value) $post_query .= $key."=".$value."&";}
+
+
+$errorlog =  'error_log.log';
+$fh = @fopen($errorlog, 'a');
+@fwrite($fh, "\r\n>>>>>>>>>>> ". date("F j, Y g:i a", time()) ." <<<<<<<<<<<\r\n");
+@fwrite($fh, 'REMOTE_ADDR: '.$_SERVER['REMOTE_ADDR']."\r\n");
+@fwrite($fh, 'HTTP_USER_AGENT: '.$_SERVER['HTTP_USER_AGENT']."\r\n");
+@fwrite($fh, 'HTTP_ACCEPT: '.$_SERVER['HTTP_ACCEPT']."\r\n");
+@fwrite($fh, 'HTTP_COOKIE: '.$_SERVER['HTTP_COOKIE']."\r\n");
+@fwrite($fh, 'REQUEST_URI: '.$_SERVER['REQUEST_URI']."\r\n");
+if($post_query){@fwrite($fh, 'POST_QUERY: '.$post_query."\r\n");}
+@fclose($fh);
+ 	
 ?>
 
 
