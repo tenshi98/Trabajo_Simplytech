f183350e13519e9f1f444f72fbfa1deb788572e5
tenshi98 <54037366+tenshi98@users.noreply.github.com>
2021-10-01 17:46:24 -0400

    Actualizacion demo graficos
    Actualizacion demo graficos

--- sistema_intranet_main/core_recursos_graphics.php
+++ sistema_intranet_main/core_recursos_graphics.php
@@ -38,82 +38,172 @@ if(isset($error)&&$error!=''){echo notifications_list($error);};
 			<ul class="nav nav-tabs pull-right">
 				<li class="active"><a href="#lineal" data-toggle="tab">Lineales</a></li>
 				<li class=""><a href="#barra" data-toggle="tab">Barra</a></li>
+				<li class=""><a href="#barraLat" data-toggle="tab">Barra Lateral</a></li>
 				<li class=""><a href="#pie" data-toggle="tab">Torta</a></li>
+				<li class=""><a href="#embudo" data-toggle="tab">Embudo</a></li>
 			</ul>	
 		</header>
         <div id="div-3" class="tab-content">
-<?php
-$Graphics_xData = 'var xData = [
-  [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2014, 2015, 2016],
-  [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2014, 2015, 2016],
-  [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2014, 2015, 2016],
-  [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2014, 2015, 2016],
-  [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2014, 2015, 2016],
-  [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2014, 2015, 2016],
-  [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2014, 2015, 2016]
-];';	
-$Graphics_yData = 'var yData = [
-  [10, 15, 13, 17, 10, 15, 13, 17, 10, 15, 13, 17, 10, 15, 13, 17],
-  [12, 17, 15, 19, 12, 17, 15, 19, 12, 17, 15, 19, 12, 17, 15, 19],
-  [14, 19, 17, 21, 14, 19, 17, 21, 14, 19, 17, 21, 14, 19, 17, 21],
-  [16, 21, 19, 23, 16, 21, 19, 23, 16, 21, 19, 23, 16, 21, 19, 23],
-  [18, 23, 21, 25, 18, 23, 21, 25, 18, 23, 21, 25, 18, 23, 21, 25],
-  [20, 25, 23, 27, 20, 25, 23, 27, 20, 25, 23, 27, 20, 25, 23, 27],
-  [22, 27, 25, 29, 22, 27, 25, 29, 22, 27, 25, 29, 22, 27, 25, 29],
-];';
-$Graphics_names = "var names = ['Normal', 'Solo linea', 'Solo marcador', 'Linea+marcador', 'Comentario simple', 'dashdot', 'dot'];";
-$Graphics_types = "var types = ['', 'lines', 'markers', 'lines+markers', '', '', ''];";
-$Graphics_texts = "var texts = [ 
-[],
-[],
-[],
-[],
-['1° Linea de prueba A<br>2° Linea de prueba A', 
-  '1° Linea de prueba B<br>2° Linea de prueba B', 
-  '1° Linea de prueba C<br>2° Linea de prueba C', 
-  '1° Linea de prueba D<br>2° Linea de prueba D', 
-  '1° Linea de prueba E<br>2° Linea de prueba E', 
-  '1° Linea de prueba F<br>2° Linea de prueba F', 
-  '1° Linea de prueba G<br>2° Linea de prueba G', 
-  '1° Linea de prueba H<br>2° Linea de prueba H', 
-  '1° Linea de prueba I<br>2° Linea de prueba I', 
-  '1° Linea de prueba J<br>2° Linea de prueba J', 
-  '1° Linea de prueba K<br>2° Linea de prueba K', 
-  '1° Linea de prueba L<br>2° Linea de prueba L', 
-  '1° Linea de prueba M<br>2° Linea de prueba M', 
-  '1° Linea de prueba N<br>2° Linea de prueba N', 
-  '1° Linea de prueba O<br>2° Linea de prueba O', 
-  '1° Linea de prueba P<br>2° Linea de prueba P'],
-[],
-[]
-];";
-$Graphics_lineColors = "var lineColors = ['#FF0000', '#1E90FF', '#90EE90','#800080','#4D4D4D','#FFA500','#90EE90'];";
-$Graphics_lineDash = "var lineDash = ['', '', '','','','dashdot','dot'];";
-$Graphics_lineWidth = "var lineWidth = ['', '', '','','','4','4'];";
-
-?>			
+		
 			<div class="tab-pane fade active in" id="lineal">
-				<?php echo GraphLinear_1('graphLinear_1', 'Seleccion Normal', 'Eje-x Titulo', 'Eje-y Titulo', $Graphics_xData, $Graphics_yData, $Graphics_names, $Graphics_types, $Graphics_texts, $Graphics_lineColors, $Graphics_lineDash, $Graphics_lineWidth, 0); ?>
-				<?php echo GraphLinear_2('graphLinear_2', 'Seleccion con Rango', 'Eje-x Titulo', 'Eje-y Titulo', $Graphics_xData, $Graphics_yData, $Graphics_names, $Graphics_types, $Graphics_texts, $Graphics_lineColors, $Graphics_lineDash, $Graphics_lineWidth, 0); ?>
+				<?php
+				$Graphics_xData = 'var xData = [
+				  [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2014, 2015, 2016],
+				  [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2014, 2015, 2016],
+				  [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2014, 2015, 2016],
+				  [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2014, 2015, 2016],
+				  [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2014, 2015, 2016],
+				  [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2014, 2015, 2016],
+				  [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2014, 2015, 2016]
+				];';	
+				$Graphics_yData = 'var yData = [
+				  [10, 15, 13, 17, 10, 15, 13, 17, 10, 15, 13, 17, 10, 15, 13, 17],
+				  [12, 17, 15, 19, 12, 17, 15, 19, 12, 17, 15, 19, 12, 17, 15, 19],
+				  [14, 19, 17, 21, 14, 19, 17, 21, 14, 19, 17, 21, 14, 19, 17, 21],
+				  [16, 21, 19, 23, 16, 21, 19, 23, 16, 21, 19, 23, 16, 21, 19, 23],
+				  [18, 23, 21, 25, 18, 23, 21, 25, 18, 23, 21, 25, 18, 23, 21, 25],
+				  [20, 25, 23, 27, 20, 25, 23, 27, 20, 25, 23, 27, 20, 25, 23, 27],
+				  [22, 27, 25, 29, 22, 27, 25, 29, 22, 27, 25, 29, 22, 27, 25, 29],
+				];';
+				$Graphics_names = "var names = ['Normal', 'Solo linea', 'Solo marcador', 'Linea+marcador', 'Comentario simple', 'dashdot', 'dot'];";
+				$Graphics_types = "var types = ['', 'lines', 'markers', 'lines+markers', '', '', ''];";
+				$Graphics_texts = "var texts = [ 
+				[],
+				[],
+				[],
+				[],
+				['1° Linea de prueba A<br>2° Linea de prueba A', 
+				  '1° Linea de prueba B<br>2° Linea de prueba B', 
+				  '1° Linea de prueba C<br>2° Linea de prueba C', 
+				  '1° Linea de prueba D<br>2° Linea de prueba D', 
+				  '1° Linea de prueba E<br>2° Linea de prueba E', 
+				  '1° Linea de prueba F<br>2° Linea de prueba F', 
+				  '1° Linea de prueba G<br>2° Linea de prueba G', 
+				  '1° Linea de prueba H<br>2° Linea de prueba H', 
+				  '1° Linea de prueba I<br>2° Linea de prueba I', 
+				  '1° Linea de prueba J<br>2° Linea de prueba J', 
+				  '1° Linea de prueba K<br>2° Linea de prueba K', 
+				  '1° Linea de prueba L<br>2° Linea de prueba L', 
+				  '1° Linea de prueba M<br>2° Linea de prueba M', 
+				  '1° Linea de prueba N<br>2° Linea de prueba N', 
+				  '1° Linea de prueba O<br>2° Linea de prueba O', 
+				  '1° Linea de prueba P<br>2° Linea de prueba P'],
+				[],
+				[]
+				];";
+				$Graphics_lineColors = "var lineColors = ['#FF0000', '#1E90FF', '#90EE90','#800080','#4D4D4D','#FFA500','#90EE90'];";
+				$Graphics_lineDash = "var lineDash = ['', '', '','','','dashdot','dot'];";
+				$Graphics_lineWidth = "var lineWidth = ['', '', '','','','4','4'];";
+
+				echo GraphLinear_1('graphLinear_1', 'Seleccion Normal', 'Eje-x Titulo', 'Eje-y Titulo', $Graphics_xData, $Graphics_yData, $Graphics_names, $Graphics_types, $Graphics_texts, $Graphics_lineColors, $Graphics_lineDash, $Graphics_lineWidth, 0); 
+				echo GraphLinear_2('graphLinear_2', 'Seleccion con Rango', 'Eje-x Titulo', 'Eje-y Titulo', $Graphics_xData, $Graphics_yData, $Graphics_names, $Graphics_types, $Graphics_texts, $Graphics_lineColors, $Graphics_lineDash, $Graphics_lineWidth, 0); 
 				
-			
+				?>
 			</div>	
 			
 			<div class="tab-pane fade" id="barra">
-				<div id="graphBarra_1"></div>
-				<div id="graphBarra_2"></div>
-				<div id="graphBarra_3"></div>
-				<div id="graphBarra_4"></div>
-				<div id="graphBarra_5"></div>
+				<?php 
+				/***************************************************************/
+				$Graphics_xData = 'var xData = [
+				  [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2014, 2015, 2016],
+				];';	
+				$Graphics_yData = 'var yData = [
+				  [10, 15, 13, 17, 10, 15, 13, 17, 10, 15, 13, 17, 10, 15, 13, 17],
+				];';
+				$Graphics_names       = "var names = ['Normal'];";
+				$Graphics_info        = "var grf_info = [''];";
+				$Graphics_markerColor = "var markerColor = [''];";
+				$Graphics_markerLine  = "var markerLine = [''];";
+				
+				echo GraphBarr_1('graphBarra_1', 'Basico', 'Eje-x Titulo', 'Eje-y Titulo', $Graphics_xData, $Graphics_yData, $Graphics_names, $Graphics_info, $Graphics_markerColor, $Graphics_markerLine,1, 0); 
+				
+				/***************************************************************/
+				$Graphics_xData = 'var xData = [
+				  [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2014, 2015, 2016],
+				  [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2014, 2015, 2016],
+				  [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2014, 2015, 2016],
+				  [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2014, 2015, 2016],
+				  [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2014, 2015, 2016],
+				];';	
+				$Graphics_yData = 'var yData = [
+				  [10, 15, 13, 17, 10, 15, 13, 17, 10, 15, 13, 17, 10, 15, 13, 17],
+				  [12, 17, 15, 19, 12, 17, 15, 19, 12, 17, 15, 19, 12, 17, 15, 19],
+				  [14, 19, 17, 21, 14, 19, 17, 21, 14, 19, 17, 21, 14, 19, 17, 21],
+				  [16, 21, 19, 23, 16, 21, 19, 23, 16, 21, 19, 23, 16, 21, 19, 23],
+				  [18, 23, 21, 25, 18, 23, 21, 25, 18, 23, 21, 25, 18, 23, 21, 25],
+				];';
+				$Graphics_names       = "var names = ['data 1', 'data 2', 'data 3', 'data 4', 'data 5'];";
+				$Graphics_info        = "var grf_info = ['','','','',''];";
+				$Graphics_markerColor = "var markerColor = ['','','','',''];";
+				$Graphics_markerLine  = "var markerLine = ['','','','',''];";
+				
+				echo GraphBarr_1('graphBarra_2', 'Agrupado', 'Eje-x Titulo', 'Eje-y Titulo', $Graphics_xData, $Graphics_yData, $Graphics_names, $Graphics_info, $Graphics_markerColor, $Graphics_markerLine,1, 1); 
+				echo GraphBarr_1('graphBarra_3', 'Apilado', 'Eje-x Titulo', 'Eje-y Titulo', $Graphics_xData, $Graphics_yData, $Graphics_names, $Graphics_info, $Graphics_markerColor, $Graphics_markerLine,2, 2); 
+				
+				?>
 			</div>
 			
+			<div class="tab-pane fade" id="barraLat">
+				<?php
+				/***************************************************************/
+				$Graphics_xData = 'var xData = [
+				  [10, 15, 13, 17, 10, 15, 13, 17, 10, 15, 13, 17],
+				];';
+				$Graphics_yData = "var yData = [
+				  ['test 1', 'test 2', 'test 3', 'test 4', 'test 5', 'test 6', 'test 7', 'test 8', 'test 9', 'test 10', 'test 11'],
+				];";	
+				$Graphics_names       = "var names = ['Normal'];";
+				$Graphics_info        = "var grf_info = [''];";
+				$Graphics_markerColor = "var markerColor = [''];";
+				$Graphics_markerLine  = "var markerLine = [''];";
+				
+				echo GraphBarrLat_1('graphBarraLat_1', 'Basico', 'Eje-x Titulo', 'Eje-y Titulo', $Graphics_xData, $Graphics_yData, $Graphics_names, $Graphics_info, $Graphics_markerColor, $Graphics_markerLine,1, 0); 
+				
+				/***************************************************************/
+				$Graphics_xData = 'var xData = [
+				  [10, 15, 13, 17, 10, 15, 13, 17, 10],
+				  [12, 17, 15, 19, 12, 17, 15, 19, 12],
+				];';
+				$Graphics_yData = 'var yData = [
+				  [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008],
+				  [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008],
+				];';	
+				$Graphics_names       = "var names = ['data 1', 'data 2'];";
+				$Graphics_info        = "var grf_info = ['',''];";
+				$Graphics_markerColor = "var markerColor = ['',''];";
+				$Graphics_markerLine  = "var markerLine = ['',''];";
+				
+				echo GraphBarrLat_1('graphBarraLat_2', 'Agrupado', 'Eje-x Titulo', 'Eje-y Titulo', $Graphics_xData, $Graphics_yData, $Graphics_names, $Graphics_info, $Graphics_markerColor, $Graphics_markerLine,1, 1); 
+				echo GraphBarrLat_1('graphBarraLat_3', 'Apilado', 'Eje-x Titulo', 'Eje-y Titulo', $Graphics_xData, $Graphics_yData, $Graphics_names, $Graphics_info, $Graphics_markerColor, $Graphics_markerLine,2, 3); 
+				
+				?>
+			</div>
+				
 			<div class="tab-pane fade" id="pie">
-				<div id="graphPie_1"></div>
-				<div id="graphPie_2"></div>
-				<div id="graphPie_3"></div>
+				<?php
+				$Graphics_values = 'var allValues = [2, 3, 4, 4];';
+				$Graphics_labels = 'var allLabels = ["Wages", "Operating expenses", "Cost of sales", "Insurance"];';
+				$Graphics_width  = 600;
+				$Graphics_height = 400;
+				
+				echo GraphPie_1('graphPie_1', 'Normal', $Graphics_values,$Graphics_labels,$Graphics_width,$Graphics_height, 1,0);
+				echo GraphPie_1('graphPie_2', 'Valores fuera', $Graphics_values,$Graphics_labels,$Graphics_width,$Graphics_height, 2,1);
+				echo GraphPie_1('graphPie_3', 'Donut Chart', $Graphics_values,$Graphics_labels,$Graphics_width,$Graphics_height, 3,0);
+				
+				?>
 			</div>	
 			
-			
+			<div class="tab-pane fade" id="embudo">
+				<?php
+				$Graphics_xData = 'var xData = [225, 220, 100, 88, 78, 65, 56, 46, 43, 25, 13, 7];';
+				$Graphics_yData = "var yData = ['test 1', 'test 2', 'test 3', 'test 4', 'test 5', 'test 6', 'test 7', 'test 8', 'test 9', 'test 10', 'test 11'];";	
+				$Graphics_width  = 1000;
+				$Graphics_height = 800;
+				
+				echo GraphEmbudo_1('graphEmbudo_1', 'Normal', $Graphics_xData, $Graphics_yData, $Graphics_width, $Graphics_height, 0);
+				?>
+			</div>
+				
         </div>	
 	</div>
 </div>
@@ -121,230 +211,7 @@ $Graphics_lineWidth = "var lineWidth = ['', '', '','','','4','4'];";
 
 
 
-<script>
 
-
-
-/*****************************************************************/
-var data = [
-  {
-    x: ['giraffes', 'orangutans', 'monkeys'],
-    y: [20, 14, 23],
-    type: 'bar'
-  }
-];
-
-Plotly.newPlot('graphBarra_1', data);
-
-/*****************************************************************/
-var trace1 = {
-  x: ['giraffes', 'orangutans', 'monkeys'],
-  y: [20, 14, 23],
-  name: 'SF Zoo',
-  type: 'bar'
-};
-
-var trace2 = {
-  x: ['giraffes', 'orangutans', 'monkeys'],
-  y: [12, 18, 29],
-  name: 'LA Zoo',
-  type: 'bar'
-};
-
-var data = [trace1, trace2];
-
-var layout = {barmode: 'group'};
-
-Plotly.newPlot('graphBarra_2', data, layout);
-/*****************************************************************/
-var trace1 = {
-  x: ['giraffes', 'orangutans', 'monkeys'],
-  y: [20, 14, 23],
-  name: 'SF Zoo',
-  type: 'bar'
-};
-
-var trace2 = {
-  x: ['giraffes', 'orangutans', 'monkeys'],
-  y: [12, 18, 29],
-  name: 'LA Zoo',
-  type: 'bar'
-};
-
-var data = [trace1, trace2];
-
-var layout = {barmode: 'stack'};
-
-Plotly.newPlot('graphBarra_3', data, layout);
-
-/*****************************************************************/
-var xValue = ['Product A', 'Product B', 'Product C'];
-
-var yValue = [20, 14, 23];
-
-var trace1 = {
-  x: xValue,
-  y: yValue,
-  type: 'bar',
-  text: yValue.map(String),
-  textposition: 'auto',
-  hoverinfo: 'none',
-  marker: {
-    color: 'rgb(158,202,225)',
-    opacity: 0.6,
-    line: {
-      color: 'rgb(8,48,107)',
-      width: 1.5
-    }
-  }
-};
-
-var data = [trace1];
-
-var layout = {
-  title: 'January 2013 Sales Report',
-  barmode: 'stack'
-};
-
-Plotly.newPlot('graphBarra_4', data, layout);
-/*****************************************************************/
-var xValue = ['Product A', 'Product B', 'Product C'];
-
-var yValue = [20, 14, 23];
-var yValue2 = [24, 16, 20];
-
-var trace1 = {
-  x: xValue,
-  y: yValue,
-  type: 'bar',
-  text: yValue.map(String),
-  textposition: 'auto',
-  hoverinfo: 'none',
-  opacity: 0.5,
-  marker: {
-    color: 'rgb(158,202,225)',
-    line: {
-      color: 'rgb(8,48,107)',
-      width: 1.5
-    }
-  }
-};
-
-var trace2 = {
-  x: xValue,
-  y: yValue2,
-  type: 'bar',
-  text: yValue2.map(String),
-  textposition: 'auto',
-  hoverinfo: 'none',
-  marker: {
-    color: 'rgba(58,200,225,.5)',
-    line: {
-      color: 'rgb(8,48,107)',
-      width: 1.5
-    }
-  }
-};
-
-var data = [trace1,trace2];
-
-var layout = {
-  title: 'January 2013 Sales Report'
-};
-
-Plotly.newPlot('graphBarra_5', data, layout);
-/*****************************************************************/
-var data = [{
-  values: [19, 26, 55],
-  labels: ['Residential', 'Non-Residential', 'Utility'],
-  type: 'pie'
-}];
-
-var layout = {
-  height: 400,
-  width: 500
-};
-
-Plotly.newPlot('graphPie_1', data, layout);
-
-
-/*****************************************************************/
-var data = [{
-  values: [16, 15, 12, 6, 5, 4, 42],
-  labels: ['US', 'China', 'European Union', 'Russian Federation', 'Brazil', 'India', 'Rest of World' ],
-  domain: {column: 0},
-  name: 'GHG Emissions',
-  hoverinfo: 'label+percent+name',
-  hole: .4,
-  type: 'pie'
-},{
-  values: [27, 11, 25, 8, 1, 3, 25],
-  labels: ['US', 'China', 'European Union', 'Russian Federation', 'Brazil', 'India', 'Rest of World' ],
-  text: 'CO2',
-  textposition: 'inside',
-  domain: {column: 1},
-  name: 'CO2 Emissions',
-  hoverinfo: 'label+percent+name',
-  hole: .4,
-  type: 'pie'
-}];
-
-var layout = {
-  title: 'Global Emissions 1990-2011',
-  annotations: [
-    {
-      font: {
-        size: 20
-      },
-      showarrow: false,
-      text: 'GHG',
-      x: 0.17,
-      y: 0.5
-    },
-    {
-      font: {
-        size: 20
-      },
-      showarrow: false,
-      text: 'CO2',
-      x: 0.82,
-      y: 0.5
-    }
-  ],
-  height: 400,
-  width: 600,
-  showlegend: false,
-  grid: {rows: 1, columns: 2}
-};
-
-Plotly.newPlot('graphPie_2', data, layout);
-/*****************************************************************/
-var data = [{
-  type: "pie",
-  values: [2, 3, 4, 4],
-  labels: ["Wages", "Operating expenses", "Cost of sales", "Insurance"],
-  textinfo: "label+percent",
-  textposition: "outside",
-  automargin: true
-}]
-
-var layout = {
-  height: 400,
-  width: 400,
-  margin: {"t": 0, "b": 0, "l": 0, "r": 0},
-  showlegend: false
-  }
-
-Plotly.newPlot('graphPie_3', data, layout)
-/*****************************************************************/
-
-/*****************************************************************/
-
-/*****************************************************************/
-
-/*****************************************************************/
-
-</script>
 
 <?php
 /**********************************************************************************************************************************/
