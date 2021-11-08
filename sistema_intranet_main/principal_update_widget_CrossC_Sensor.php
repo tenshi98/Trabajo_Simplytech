a9d50ce6270886ee3b132a2624f1ab8ed9fd6898
tenshi98 <54037366+tenshi98@users.noreply.github.com>
2021-11-08 15:52:15 -0400

    Cambio visualizacion
    Cambio visualizacion

--- sistema_intranet_main/principal_update_widget_CrossC_Sensor.php
+++ sistema_intranet_main/principal_update_widget_CrossC_Sensor.php
@@ -136,6 +136,7 @@ foreach($arrMediciones as $cli) {
 }
 	
 //variables
+$x_graph_count        = 0;
 $Graphics_xData       = 'var xData = [';
 $Graphics_yData       = 'var yData = [';
 $Graphics_names       = 'var names = [';
@@ -163,6 +164,8 @@ for ($x = 1; $x <= $N_Maximo_Sensores; $x++) {
 		$Graphics_lineDash   .= "'',";
 		//los anchos de la linea
 		$Graphics_lineWidth  .= "'',";
+		//contador
+		$x_graph_count++;
 	}
 } 
 $Graphics_xData      .= '];';
@@ -177,12 +180,19 @@ $Graphics_lineWidth  .= '];';
 $widget = '
 <script type="text/javascript" src="'.DB_SITE_REPO.'/LIBS_js/plotly_js/dist/plotly.min.js"></script>
 <script type="text/javascript" src="'.DB_SITE_REPO.'/LIBS_js/plotly_js/dist/plotly-locale-es-ar.js"></script>
-';			
+';		
 
-$gr_tittle = 'Grafico '.$arrGruposTemp[$idGrupo];
-$gr_unimed = '°C';
-$widget .= GraphLinear_1('graphLinear_1', $gr_tittle, 'Fecha', $gr_unimed, $Graphics_xData, $Graphics_yData, $Graphics_names, $Graphics_types, $Graphics_texts, $Graphics_lineColors, $Graphics_lineDash, $Graphics_lineWidth, 1);
-		
+//si hay datos
+if(isset($x_graph_count)&&$x_graph_count!=0){
+	$gr_tittle = 'Grafico '.$arrGruposTemp[$idGrupo];
+	$gr_unimed = '°C';
+	$widget .= GraphLinear_1('graphLinear_1', $gr_tittle, 'Fecha', $gr_unimed, $Graphics_xData, $Graphics_yData, $Graphics_names, $Graphics_types, $Graphics_texts, $Graphics_lineColors, $Graphics_lineDash, $Graphics_lineWidth, 1);
+//si no hay datos	
+}else{
+	$widget .= '<div class="col-sm-12"><br/>';
+	$widget .= '<div class="alert alert-danger alert-white rounded alert_box_correction" role="alert"><div class="icon"><i class="fa fa-info-circle faa-bounce animated" aria-hidden="true"></i></div><span id="alert_post_data">No hay datos para desplegar el grafico</span><div class="clearfix"></div></div>';
+	$widget .= '</div>';
+}			
 
 echo $widget;
 ?>
