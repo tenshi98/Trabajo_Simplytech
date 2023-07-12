5babd628bfdab8d4017802720bb77c6ce2a7f339
Victor Reyes <tenshi98@gmail.com>
2023-07-12 11:39:57 -0400

    Actualizacion transaccion
    Actualizacion transaccion

--- sistema_intranet_main/informe_telemetria_registro_sensores_4.php
+++ sistema_intranet_main/informe_telemetria_registro_sensores_4.php
@@ -24,164 +24,157 @@ require_once 'core/Web.Header.Main.php';
 /**********************************************************************************************************************************/
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 if(!empty($_GET['submit_filter'])){
-
-             
-  
-/*********************************************************************************/
-//Verifico si se selecciono el equipo
-if(isset($_GET['idTelemetria'])&&$_GET['idTelemetria']!=''){
-	//Se traen todos los registros
-	$SIS_query = 'telemetria_listado.Nombre,
-	COUNT(telemetria_listado_tablarelacionada_'.$_GET['idTelemetria'].'.idTabla) AS Total';
-	$SIS_join  = 'LEFT JOIN `telemetria_listado` ON telemetria_listado.idTelemetria = telemetria_listado_tablarelacionada_'.$_GET['idTelemetria'].'.idTelemetria';
-	$SIS_where = '(telemetria_listado_tablarelacionada_'.$_GET['idTelemetria'].'.FechaSistema BETWEEN "'.$_GET['f_inicio'].'" AND "'.$_GET['f_termino'].'")';
-	$row_data = db_select_data (false, $SIS_query, 'telemetria_listado_tablarelacionada_'.$_GET['idTelemetria'], $SIS_join, $SIS_where, $dbConn, $_SESSION['usuario']['basic_data']['Nombre'], $original, 'row_data');
-
-	/*****************************************/
-	//Se escribe el dato
-	$Alert_Text  = 'Total de registros encontrados de '.$row_data['Nombre'].': '.Cantidades($row_data['Total'], 0);
-	alert_post_data(1,1,1, $Alert_Text);
-
-
-	$total_files = ceil($row_data['Total']/5000);
-	for ($i = 1; $i <= $total_files; $i++) {
-		$reg_ini = (5000*$i)-4999;
-		$reg_fin = 5000*$i;
-		$datosx  = '&idTelemetria='.$_GET['idTelemetria'];
-		$datosx .= '&f_inicio='.$_GET['f_inicio'];
-		$datosx .= '&f_termino='.$_GET['f_termino'];
-		$datosx .= '&num='.$i;
-
-		$Alert_Text  = '<span class="pull-left">Exportar archivo '.$i.' registros del '.Cantidades($reg_ini, 0).' al '.Cantidades($reg_fin, 0).'</span>';
-		$Alert_Text .= '<a target="new" href="informe_telemetria_registro_sensores_4_to_excel.php?bla=bla'.$datosx.'" class="btn btn-sm btn-metis-2 pull-right "><i class="fa fa-file-excel-o" aria-hidden="true"></i> Exportar a Excel</a>';
-		$Alert_Text .= '<div class="clearfix"></div>';
-		alert_post_data(2,1,1, $Alert_Text);
-	  
-	}
-//Si no se slecciono se traen todos los equipos a los cuales tiene permiso
-}else{
-	//Inicia variable
-	$SIS_where = "telemetria_listado.idTelemetria>0";
-	$SIS_where.= " AND telemetria_listado.id_Geo='2'";
-	$SIS_where.= " AND telemetria_listado.idSistema=".$_SESSION['usuario']['basic_data']['idSistema'];
-	//Solo para plataforma CrossTech
-	if(isset($_SESSION['usuario']['basic_data']['idInterfaz'])&&$_SESSION['usuario']['basic_data']['idInterfaz']==6){
-		$SIS_where .= " AND telemetria_listado.idTab=2";//CrossC
-	}
-	$datosx  = '&f_inicio='.$_GET['f_inicio'];
-	$datosx .= '&f_termino='.$_GET['f_termino'];
-	$datosx .= '&idTipoUsuario='.$_SESSION['usuario']['basic_data']['idTipoUsuario'];
-	$datosx .= '&idSistema='.$_SESSION['usuario']['basic_data']['idSistema'];
-	$datosx .= '&idUsuario='.$_SESSION['usuario']['basic_data']['idUsuario'];
-
-	//Verifico el tipo de usuario que esta ingresando
-	$SIS_join  = '';
-	if($_SESSION['usuario']['basic_data']['idTipoUsuario']!=1){
-		$SIS_join .= " INNER JOIN usuarios_equipos_telemetria ON usuarios_equipos_telemetria.idTelemetria = telemetria_listado.idTelemetria ";
-		$SIS_where.= ' AND usuarios_equipos_telemetria.idUsuario='.$_SESSION['usuario']['basic_data']['idUsuario'];
-	}
-
-	/*********************************************/
-	// Se trae un listado con todos los elementos
-	$SIS_query = 'idTelemetria, Nombre';
-	$SIS_order = 'idTelemetria ASC';
-	$arrEquipos = array();
-	$arrEquipos = db_select_array (false, $SIS_query, 'telemetria_listado', $SIS_join, $SIS_where, $SIS_order, $dbConn, $_SESSION['usuario']['basic_data']['Nombre'], $original, 'arrEquipos');
-
-	/*********************************************/
-	$s_max = 0;
-	$Alert_Text  = '';
-	foreach ($arrEquipos as $equipo) {
+	/*********************************************************************************/
+	//Verifico si se selecciono el equipo
+	if(isset($_GET['idTelemetria'])&&$_GET['idTelemetria']!=''){
 		//Se traen todos los registros
-		$SIS_query = 'COUNT(idTabla) AS Total';
-		$SIS_where = '(FechaSistema BETWEEN "'.$_GET['f_inicio'].'" AND "'.$_GET['f_termino'].'")';
-		$row_data = db_select_data (false, $SIS_query, 'telemetria_listado_tablarelacionada_'.$equipo['idTelemetria'], '', $SIS_where, $dbConn, $_SESSION['usuario']['basic_data']['Nombre'], $original, 'row_data');
+		$SIS_query = 'telemetria_listado.Nombre,
+		COUNT(telemetria_listado_tablarelacionada_'.$_GET['idTelemetria'].'.idTabla) AS Total';
+		$SIS_join  = 'LEFT JOIN `telemetria_listado` ON telemetria_listado.idTelemetria = telemetria_listado_tablarelacionada_'.$_GET['idTelemetria'].'.idTelemetria';
+		$SIS_where = '(telemetria_listado_tablarelacionada_'.$_GET['idTelemetria'].'.FechaSistema BETWEEN "'.$_GET['f_inicio'].'" AND "'.$_GET['f_termino'].'")';
+		$row_data = db_select_data (false, $SIS_query, 'telemetria_listado_tablarelacionada_'.$_GET['idTelemetria'], $SIS_join, $SIS_where, $dbConn, $_SESSION['usuario']['basic_data']['Nombre'], $original, 'row_data');
 
-		$Alert_Text .= 'Total de registros encontrados de '.$equipo['Nombre'].': '.Cantidades($row_data['Total'], 0).'<br/>';
-		//verifico el valor maximo
-		if($s_max<$row_data['Total']){
-			$s_max=$row_data['Total'];
+		/*****************************************/
+		//Se escribe el dato
+		$Alert_Text  = 'Total de registros encontrados de '.$row_data['Nombre'].': '.Cantidades($row_data['Total'], 0);
+		alert_post_data(1,1,1, $Alert_Text);
+
+		$total_files = ceil($row_data['Total']/5000);
+		for ($i = 1; $i <= $total_files; $i++) {
+			$reg_ini = (5000*$i)-4999;
+			$reg_fin = 5000*$i;
+			$datosx  = '&idTelemetria='.$_GET['idTelemetria'];
+			$datosx .= '&f_inicio='.$_GET['f_inicio'];
+			$datosx .= '&f_termino='.$_GET['f_termino'];
+			$datosx .= '&num='.$i;
+
+			$Alert_Text  = '<span class="pull-left">Exportar archivo '.$i.' registros del '.Cantidades($reg_ini, 0).' al '.Cantidades($reg_fin, 0).'</span>';
+			$Alert_Text .= '<a target="new" href="informe_telemetria_registro_sensores_4_to_excel.php?bla=bla'.$datosx.'" class="btn btn-sm btn-metis-2 pull-right "><i class="fa fa-file-excel-o" aria-hidden="true"></i> Exportar a Excel</a>';
+			$Alert_Text .= '<div class="clearfix"></div>';
+			alert_post_data(2,1,1, $Alert_Text);
 		}
-	}
-	alert_post_data(2,1,1, $Alert_Text);
-	
-	
-	/*****************************************/
-	//Se escribe el dato
-	$total_files = ceil($s_max/5000);
-	for ($i = 1; $i <= $total_files; $i++) {
-		$reg_ini = (5000*$i)-4999;
-		$reg_fin = 5000*$i;
+	//Si no se slecciono se traen todos los equipos a los cuales tiene permiso
+	}else{
+		//Inicia variable
+		$SIS_where = "telemetria_listado.idTelemetria>0";
+		$SIS_where.= " AND telemetria_listado.id_Geo='2'";
+		$SIS_where.= " AND telemetria_listado.idSistema=".$_SESSION['usuario']['basic_data']['idSistema'];
+		//Solo para plataforma CrossTech
+		if(isset($_SESSION['usuario']['basic_data']['idInterfaz'])&&$_SESSION['usuario']['basic_data']['idInterfaz']==6){
+			$SIS_where .= " AND telemetria_listado.idTab=2";//CrossC
+		}
+		$datosx  = '&f_inicio='.$_GET['f_inicio'];
+		$datosx .= '&f_termino='.$_GET['f_termino'];
+		$datosx .= '&idTipoUsuario='.$_SESSION['usuario']['basic_data']['idTipoUsuario'];
+		$datosx .= '&idSistema='.$_SESSION['usuario']['basic_data']['idSistema'];
+		$datosx .= '&idUsuario='.$_SESSION['usuario']['basic_data']['idUsuario'];
 
-		$datosx .= '&num='.$i;
+		//Verifico el tipo de usuario que esta ingresando
+		$SIS_join  = '';
+		if($_SESSION['usuario']['basic_data']['idTipoUsuario']!=1){
+			$SIS_join .= " INNER JOIN usuarios_equipos_telemetria ON usuarios_equipos_telemetria.idTelemetria = telemetria_listado.idTelemetria ";
+			$SIS_where.= ' AND usuarios_equipos_telemetria.idUsuario='.$_SESSION['usuario']['basic_data']['idUsuario'];
+		}
 
-		$Alert_Text  = '<span class="pull-left">Exportar archivo '.$i.' registros del '.Cantidades($reg_ini, 0).' al '.Cantidades($reg_fin, 0).'</span>';
-		$Alert_Text .= '<a target="new" href="informe_telemetria_registro_sensores_4_to_excel.php?bla=bla'.$datosx.'" class="btn btn-sm btn-metis-2 pull-right "><i class="fa fa-file-excel-o" aria-hidden="true"></i> Exportar a Excel</a>';
-		$Alert_Text .= '<div class="clearfix"></div>';
+		/*********************************************/
+		// Se trae un listado con todos los elementos
+		$SIS_query = '
+		telemetria_listado.idTelemetria,
+		telemetria_listado.Nombre';
+		$SIS_order = 'telemetria_listado.idTelemetria ASC';
+		$arrEquipos = array();
+		$arrEquipos = db_select_array (false, $SIS_query, 'telemetria_listado', $SIS_join, $SIS_where, $SIS_order, $dbConn, $_SESSION['usuario']['basic_data']['Nombre'], $original, 'arrEquipos');
+
+		/*********************************************/
+		$s_max = 0;
+		$Alert_Text  = '';
+		foreach ($arrEquipos as $equipo) {
+			//Se traen todos los registros
+			$SIS_query = 'COUNT(idTabla) AS Total';
+			$SIS_where = '(FechaSistema BETWEEN "'.$_GET['f_inicio'].'" AND "'.$_GET['f_termino'].'")';
+			$row_data = db_select_data (false, $SIS_query, 'telemetria_listado_tablarelacionada_'.$equipo['idTelemetria'], '', $SIS_where, $dbConn, $_SESSION['usuario']['basic_data']['Nombre'], $original, 'row_data');
+
+			$Alert_Text .= 'Total de registros encontrados de '.$equipo['Nombre'].': '.Cantidades($row_data['Total'], 0).'<br/>';
+			//verifico el valor maximo
+			if($s_max<$row_data['Total']){
+				$s_max=$row_data['Total'];
+			}
+		}
 		alert_post_data(2,1,1, $Alert_Text);
-		  
+
+		/*****************************************/
+		//Se escribe el dato
+		$total_files = ceil($s_max/5000);
+		for ($i = 1; $i <= $total_files; $i++) {
+			$reg_ini = (5000*$i)-4999;
+			$reg_fin = 5000*$i;
+
+			$datosx .= '&num='.$i;
+
+			$Alert_Text  = '<span class="pull-left">Exportar archivo '.$i.' registros del '.Cantidades($reg_ini, 0).' al '.Cantidades($reg_fin, 0).'</span>';
+			$Alert_Text .= '<a target="new" href="informe_telemetria_registro_sensores_4_to_excel.php?bla=bla'.$datosx.'" class="btn btn-sm btn-metis-2 pull-right "><i class="fa fa-file-excel-o" aria-hidden="true"></i> Exportar a Excel</a>';
+			$Alert_Text .= '<div class="clearfix"></div>';
+			alert_post_data(2,1,1, $Alert_Text);
+		}
+
 	}
 
-}
+	?>
 
-?>
-
-<div class="clearfix"></div>
-<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="margin-bottom:30px">
-	<a href="<?php echo $location ?>" class="btn btn-danger pull-right"><i class="fa fa-arrow-left" aria-hidden="true"></i> Volver</a>
 	<div class="clearfix"></div>
-</div>
+	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="margin-bottom:30px">
+		<a href="<?php echo $location ?>" class="btn btn-danger pull-right"><i class="fa fa-arrow-left" aria-hidden="true"></i> Volver</a>
+		<div class="clearfix"></div>
+	</div>
 
 <?php //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 } else {
-//Filtro de busqueda
-$z  = "telemetria_listado.idSistema=".$_SESSION['usuario']['basic_data']['idSistema'];   //Sistema
-$z .= " AND telemetria_listado.id_Geo=2";                                                //Geolocalizacion inactiva
-//Verifico el tipo de usuario que esta ingresando
-if($_SESSION['usuario']['basic_data']['idTipoUsuario']!=1){
-	$z .= " AND usuarios_equipos_telemetria.idUsuario = ".$_SESSION['usuario']['basic_data']['idUsuario'];
-}
-//Solo para plataforma CrossTech
-if(isset($_SESSION['usuario']['basic_data']['idInterfaz'])&&$_SESSION['usuario']['basic_data']['idInterfaz']==6){
-	$z .= " AND telemetria_listado.idTab=2";//CrossC
-} ?>
-<div class="col-xs-12 col-sm-10 col-md-9 col-lg-8 fcenter">
-	<div class="box dark">
-		<header>
-			<div class="icons"><i class="fa fa-edit" aria-hidden="true"></i></div>
-			<h5>Filtro de busqueda</h5>
-		</header>
-		<div class="body">
-			<form class="form-horizontal" id="form1" name="form1" action="<?php echo $location; ?>" autocomplete="off" novalidate>
+	//Filtro de busqueda
+	$z  = "telemetria_listado.idSistema=".$_SESSION['usuario']['basic_data']['idSistema'];   //Sistema
+	$z .= " AND telemetria_listado.id_Geo=2";                                                //Geolocalizacion inactiva
+	//Verifico el tipo de usuario que esta ingresando
+	if($_SESSION['usuario']['basic_data']['idTipoUsuario']!=1){
+		$z .= " AND usuarios_equipos_telemetria.idUsuario = ".$_SESSION['usuario']['basic_data']['idUsuario'];
+	}
+	//Solo para plataforma CrossTech
+	if(isset($_SESSION['usuario']['basic_data']['idInterfaz'])&&$_SESSION['usuario']['basic_data']['idInterfaz']==6){
+		$z .= " AND telemetria_listado.idTab=2";//CrossC
+	} ?>
+	<div class="col-xs-12 col-sm-10 col-md-9 col-lg-8 fcenter">
+		<div class="box dark">
+			<header>
+				<div class="icons"><i class="fa fa-edit" aria-hidden="true"></i></div>
+				<h5>Filtro de busqueda</h5>
+			</header>
+			<div class="body">
+				<form class="form-horizontal" id="form1" name="form1" action="<?php echo $location; ?>" autocomplete="off" novalidate>
 
-				<?php
-				//Se verifican si existen los datos
-				if(isset($f_inicio)){      $x1  = $f_inicio;     }else{$x1  = '';}
-				if(isset($f_termino)){     $x2  = $f_termino;    }else{$x2  = '';}
-				if(isset($idTelemetria)){  $x3  = $idTelemetria; }else{$x3  = '';}
+					<?php
+					//Se verifican si existen los datos
+					if(isset($f_inicio)){      $x1  = $f_inicio;     }else{$x1  = '';}
+					if(isset($f_termino)){     $x2  = $f_termino;    }else{$x2  = '';}
+					if(isset($idTelemetria)){  $x3  = $idTelemetria; }else{$x3  = '';}
 
-				//se dibujan los inputs
-				$Form_Inputs = new Form_Inputs();
-				$Form_Inputs->form_date('Fecha Inicio','f_inicio', $x1, 2);
-				$Form_Inputs->form_date('Fecha Termino','f_termino', $x2, 2);
-				//Verifico el tipo de usuario que esta ingresando
-				if($_SESSION['usuario']['basic_data']['idTipoUsuario']==1){
-					$Form_Inputs->form_select_filter('Equipo','idTelemetria', $x3, 1, 'idTelemetria', 'Nombre', 'telemetria_listado', $z, '', $dbConn);
-				}else{
-					$Form_Inputs->form_select_join_filter('Equipo','idTelemetria', $x3, 1, 'idTelemetria', 'Nombre', 'telemetria_listado', 'usuarios_equipos_telemetria', $z, $dbConn);
-				} ?>        
-	   
-				
-				<div class="form-group">
-					<input type="submit" class="btn btn-primary pull-right margin_form_btn fa-input" value="&#xf002; Filtrar" name="submit_filter">
-				</div>
-			</form>
-			<?php widget_validator(); ?>
+					//se dibujan los inputs
+					$Form_Inputs = new Form_Inputs();
+					$Form_Inputs->form_date('Fecha Inicio','f_inicio', $x1, 2);
+					$Form_Inputs->form_date('Fecha Termino','f_termino', $x2, 2);
+					//Verifico el tipo de usuario que esta ingresando
+					if($_SESSION['usuario']['basic_data']['idTipoUsuario']==1){
+						$Form_Inputs->form_select_filter('Equipo','idTelemetria', $x3, 1, 'idTelemetria', 'Nombre', 'telemetria_listado', $z, '', $dbConn);
+					}else{
+						$Form_Inputs->form_select_join_filter('Equipo','idTelemetria', $x3, 1, 'idTelemetria', 'Nombre', 'telemetria_listado', 'usuarios_equipos_telemetria', $z, $dbConn);
+					} ?>
+
+					<div class="form-group">
+						<input type="submit" class="btn btn-primary pull-right margin_form_btn fa-input" value="&#xf002; Filtrar" name="submit_filter">
+					</div>
+				</form>
+				<?php widget_validator(); ?>
+			</div>
 		</div>
 	</div>
-</div>
 <?php } ?>
-
 <?php
 /**********************************************************************************************************************************/
 /*                                             Se llama al pie del documento html                                                 */
