7a4d90c85fce1a4ce1b0eb4ac6ccf5b1eaa9eb42
Victor Reyes Galvez <54037366+tenshi98@users.noreply.github.com>
2024-09-26 13:32:47 -0300

    Se crea nuevo cron
    Se crea nuevo cron

---
+++ web_app/main_ardu/cron_inf_proyecciones.php
@@ -0,0 +1,96 @@
+<?php
+/**********************************************************************************************************************************/
+/*                                                   Se define la Sesion                                                          */
+/**********************************************************************************************************************************/
+$timeout = 604800;                               //Se setea la expiracion a una semana
+ini_set( "session.gc_maxlifetime", $timeout );   //Establecer la vida útil máxima de la sesión
+ini_set( "session.cookie_lifetime", $timeout );  //Establecer la duración de las cookies de la sesión
+session_start();                                 //Iniciar una nueva sesión
+/**********************************************************************************************************************************/
+/*                                           Se define la variable de seguridad                                                   */
+/**********************************************************************************************************************************/
+define('XMBCXRXSKGC', 1);
+/**********************************************************************************************************************************/
+/*                                          Se llaman a los archivos necesarios                                                   */
+/**********************************************************************************************************************************/
+require_once 'A1XRXS_sys/xrxs_configuracion/config.php';                                   //Configuracion de la plataforma
+require_once '../../Legacy/gestion_modular/funciones/Helpers.Functions.Propias.ardu.php';  //carga librerias de la plataforma
+
+// obtengo puntero de conexion con la db
+$dbConn = conectar();
+//Se elimina la restriccion del sql 5.7
+mysqli_query($dbConn, "SET SESSION sql_mode = ''");
+/**********************************************************************************************************************************/
+/*                                               FUNCION DEL CRON                                                                 */
+/**********************************************************************************************************************************/
+//Envia informes cada 1 hora
+/**********************************************************************************************************************************/
+/*                                       VARIABLES GLOBALES DE CONFIGURACION                                                      */
+/**********************************************************************************************************************************/
+//Tiempo Maximo de la consulta, 40 minutos por defecto
+set_time_limit(2400);
+//Memora RAM Maxima del servidor, 4GB por defecto
+ini_set('memory_limit', '4096M');
+
+//Libreria
+require_once '../../LIBS_php/PHP_ML/vendor/autoload.php';
+
+/***********************************************************************/
+//Variables
+$FechaSistema   = fecha_actual();
+$HoraSistema    = hora_actual();
+$HoraProyectada = sumahoras($HoraSistema, '01:00:00');
+
+/***********************************************************************/
+//Areglo con los equipos y el numero de predicciones
+$arrPrevs = array(
+    array(
+        "EquipoID"     => 252,
+        "NPrediccion"  => 40,
+        "RangoInicio"  => 18,
+        "RangoTermino" => 22,
+        "Sensores"     => array(1, 4, 7, 13, 16, 19, 22, 25, 28, 31, 34),
+    ),
+);
+
+/***********************************************************************/
+//Se buscan los correos de los usuarios que tengan permiso de visualizacion de los equipos
+$SIS_query = '
+telemetria_mnt_correos_list.idUsuario,
+telemetria_listado.idSistema,
+telemetria_listado.Nombre AS EquipoNombre,
+telemetria_mnt_correos_list.idCorreosCat,
+
+usuarios_listado.email AS UsuarioEmail,
+usuarios_listado.Nombre AS UsuarioNombre,
+usuarios_listado.Fono AS UsuarioFono,
+
+core_sistemas.Nombre AS SistemaNombre,
+core_sistemas.Config_WhatsappToken AS SistemaWhatsappToken,
+core_sistemas.Config_WhatsappInstanceId AS SistemaWhatsappInstanceId';
+$SIS_join  = '
+INNER JOIN `usuarios_equipos_telemetria`   ON usuarios_equipos_telemetria.idUsuario   = telemetria_mnt_correos_list.idUsuario
+INNER JOIN `usuarios_listado`              ON usuarios_listado.idUsuario              = telemetria_mnt_correos_list.idUsuario
+INNER JOIN `telemetria_mnt_correos_cat`    ON telemetria_mnt_correos_cat.idCorreosCat = telemetria_mnt_correos_list.idCorreosCat
+INNER JOIN `telemetria_listado`            ON telemetria_listado.idTelemetria         = usuarios_equipos_telemetria.idTelemetria
+INNER JOIN `core_sistemas`                 ON core_sistemas.idSistema                 = telemetria_listado.idSistema ';
+$SIS_where = '(telemetria_mnt_correos_list.TimeStamp<"'.$FechaSistema.' '.$HoraSistema.'" OR telemetria_mnt_correos_list.TimeStamp="0000-00-00 00:00:00")';
+$SIS_where.= ' AND telemetria_mnt_correos_cat.idEstado=1';           //Categoria activa
+$SIS_where.= ' AND telemetria_mnt_correos_list.idSistema != "" ';    //pertenezca a algun sistema
+$SIS_where.= ' AND telemetria_listado.idEstado = 1';                 //Solo equipos activos
+$SIS_where.= ' AND core_sistemas.idEstado = 1';                      //Solo sistemas activos
+$SIS_where.= ' AND usuarios_listado.idEstado = 1';                   //Solo usuarios activos
+$SIS_where.= ' AND telemetria_mnt_correos_list.idCorreosCat = 62';   //Crones - Prevision temperatura
+$SIS_where.= ' GROUP BY telemetria_mnt_correos_list.idUsuario,telemetria_listado.idSistema,usuarios_equipos_telemetria.idTelemetria,telemetria_mnt_correos_list.idCorreosCat';
+$SIS_order = 'telemetria_mnt_correos_list.idUsuario ASC,telemetria_listado.idSistema ASC,usuarios_equipos_telemetria.idTelemetria ASC,telemetria_mnt_correos_list.idCorreosCat ASC';
+$arrCorreos = array();
+$arrCorreos = db_select_array (false, $SIS_query, 'telemetria_mnt_correos_list', $SIS_join, $SIS_where, $SIS_order, $dbConn, 'cron', basename($_SERVER["REQUEST_URI"], ".php"), 'arrCorreos');
+
+/**********************************************************************************************************************************/
+/*                                                  Includes                                                                      */
+/**********************************************************************************************************************************/
+include 'cron_inf_proyecciones_include.php';   //correcciones en las mediciones
+
+
+
+?>
---
+++ web_app/main_ardu/cron_inf_proyecciones_include.php
@@ -0,0 +1,139 @@
+<?php
+foreach ($arrPrevs as $key => $value) {
+
+	/*******************************************/
+	//Se arma la consulta
+	$SIS_query = 'idTabla, idTelemetria';
+	$SIS_join  = '';
+	$SIS_where = '';
+	$SIS_order = 'idTabla DESC LIMIT '.$value['NPrediccion'];
+	//Recorro los sensores
+    foreach ($value['Sensores'] as $sen) {
+		$SIS_query .= ',Sensor_'.$sen;
+    }
+
+	//consulto
+	$arrPrevision = array();
+	$arrPrevision = db_select_array (false, $SIS_query, 'telemetria_listado_tablarelacionada_'.$value['EquipoID'], $SIS_join, $SIS_where, $SIS_order, $dbConn, 'Cron', basename($_SERVER["REQUEST_URI"], ".php"), 'cron');
+
+	//Ordeno del mayor al menor
+	sort($arrPrevision);
+
+	//Variables
+	$arrContador     = array();
+	$arrTemperatura  = array();
+	$counterEquip    = 0;
+	$Prediccion      = 0;
+
+	/*******************************************/
+	if($arrPrevision!=false){
+		/*******************************************/
+		//recorro
+		foreach ($arrPrevision as $prev) {
+			$sSuma  = 0;
+			$sCount = 0;
+			$sProm  = 0;
+			//recorro los sensores
+			foreach ($value['Sensores'] as $sen) {
+				//mientras sea inferior al error
+				if(isset($prev['Sensor_'.$sen])&&$prev['Sensor_'.$sen]<999){
+					$sSuma  = $sSuma + $prev['Sensor_'.$sen];
+					$sCount++;
+				}
+			}
+			//verifico si hay datos
+			if($sCount!=0){
+				$sProm  = $sSuma/$sCount;//obtengo promedio
+			}
+			//calculos
+			$arrContador[$counterEquip][0]   = $counterEquip;
+			$arrTemperatura[$counterEquip]   = cantidades_google(cantidades($sProm, 2));
+			$counterEquip++;
+		}
+
+		/*******************************************/
+		//Si hay datos
+		if(isset($arrTemperatura)&&isset($arrContador)&&$counterEquip>1){
+			$regression = new Phpml\Regression\LeastSquares();
+			$regression->train($arrContador, $arrTemperatura);
+			$Prediccion = $regression->predict([$value['NPrediccion']]);
+		}
+
+		/*******************************************/
+		//cadena
+		$SIS_data  = "'".$value['EquipoID']."'";
+		$SIS_data .= ",'".$FechaSistema."'";
+		$SIS_data .= ",'".$HoraSistema."'";
+		$SIS_data .= ",'".$value['NPrediccion']."'";
+		$SIS_data .= ",'".$Prediccion."'";
+
+		// inserto los datos de registro en la db
+		$SIS_columns = 'idTelemetria, FechaSistema, HoraSistema, NPrediccion, Prediccion';
+		$ultimo_id = db_insert_data (false, $SIS_columns, $SIS_data, 'telemetria_listado_prevision', $dbConn, 'Cron', basename($_SERVER["REQUEST_URI"], ".php"), 'db_insert_data');
+
+		/*******************************************/
+		//si hay alertas
+		//Si es inferior al rango inicio o superior a este
+		if($Prediccion<$value['RangoInicio']&&$Prediccion>$value['RangoTermino']){
+			//se filtra por usuario
+			filtrar($arrCorreos, 'idUsuario');
+			//se recorren correos
+			foreach($arrCorreos as $usuarios=>$correos){
+				//Datos para guardar registro del envio de la notificacion
+				$usuario_idUsuario = $usuarios;
+				//se recorren los correos del usuario
+				foreach ($correos as $correo) {
+					/*******************************************/
+					//obtengo los datos del usuario
+					$usuarioNombre          = DeSanitizar($correo['UsuarioNombre']);   //Nombre del usuario
+					$usuarioCorreo          = DeSanitizar($correo['UsuarioEmail']);    //Para el envio de correos
+					$usuarioFono            = $correo['UsuarioFono'];                  //Para el envio de whatsapp
+
+					//obtengo los datos del sistema
+					$SistemaNombre             = DeSanitizar($correo['SistemaNombre']); //Para el envio de correos
+					$SistemaWhatsappToken      = $correo['SistemaWhatsappToken'];       //Para el envio de whatsapp
+					$SistemaWhatsappInstance   = $correo['SistemaWhatsappInstanceId'];  //Para el envio de whatsapp
+
+					//Datos para guardar registro del envio de la notificacion
+					$usuario_idSistema          = $correo['idSistema'];
+					$usuario_idCorreosCat       = $correo['idCorreosCat'];
+
+					/*******************************************/
+					//se crea cuerpo de whatsapp
+					$MSG_Whatsapp .= 'La temperatura proyectada para <strong>'.DeSanitizar($correo['EquipoNombre']).'</strong>
+					a las '.$HoraProyectada.' hrs esta fuera de rango (rango '.$value['RangoInicio'].' - '.$value['RangoTermino'].')
+					con una proyeccion de '.$Prediccion.'';
+
+					/*******************************************/
+					//se intenta enviar la notificacion
+					try {
+						/************************/
+						//envio notificacion
+						WhatsappSendMessage($SistemaWhatsappToken, $SistemaWhatsappInstance, $usuarioFono, $MSG_Whatsapp);
+						//guardo el registro de los mensajes enviados
+						$dir .= "	- NW/".$SistemaNombre.": ".$usuarioCorreo." / (Envio Correcto->".$usuarioFono.")\n";
+						/************************/
+						//envio correcto
+						if(isset($usuario_idSistema) && $usuario_idSistema!=''){       $SIS_data  = "'".$usuario_idSistema."'";      }else{$SIS_data  = "''";}
+						if(isset($usuario_idUsuario) && $usuario_idUsuario!=''){       $SIS_data .= ",'".$usuario_idUsuario."'";     }else{$SIS_data .= ",''";}
+						if(isset($usuario_idCorreosCat) && $usuario_idCorreosCat!=''){ $SIS_data .= ",'".$usuario_idCorreosCat."'";  }else{$SIS_data .= ",''";}
+						//El timestamp
+						if(isset($FechaSistema) && $FechaSistema != ''&&isset($HoraSistema) && $HoraSistema!=''){
+							$SIS_data .= ",'".$FechaSistema." ".$HoraSistema."'";
+						}else{
+							$SIS_data .= ",''";
+						}
+						// inserto los datos de registro en la db
+						$SIS_columns = 'idSistema, idUsuario, idCorreosCat, TimeStamp';
+						$ultimo_id = db_insert_data (false, $SIS_columns, $SIS_data, 'telemetria_mnt_correos_list_sended', $dbConn, 'Cron', basename($_SERVER["REQUEST_URI"], ".php"), 'insertCorreoSended');
+
+					} catch (Exception $e) {
+						$dir .= "	- NW/Excepción capturada: / (Envio Noti Whatsapp Fallido->".$e->getMessage().")\n";
+					}
+				}
+			}
+		}
+	}
+}
+
+?>
