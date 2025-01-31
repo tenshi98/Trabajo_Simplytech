3fc67497b34f472a55a63ed1cd1338785a275301
Victor Reyes Galvez <54037366+tenshi98@users.noreply.github.com>
2025-01-31 07:47:16 -0300

    se actualiza get
    se actualiza get

--- web_app/main_ardu_reenvio/ardu_include_gets.php
+++ web_app/main_ardu_reenvio/ardu_include_gets.php
@@ -26,13 +26,14 @@ $Var_Counter     = 0;
 ////////////////// Recepcion variables //////////////////
 //Datos Varios
 if ( isset($_GET['lock'])){     $lock                  = str_replace('%20', '', str_replace(' ', '', $_GET['lock']));}
-if (!empty($_GET['id'])){      $Identificador         = str_replace('%20', '', str_replace(' ', '', $_GET['id']));}
-if (!empty($_GET['f'])){       $Fecha                 = str_replace('%20', '', str_replace(' ', '', $_GET['f']));}
-if (!empty($_GET['h'])){       $Hora                  = str_replace('%20', '', str_replace(' ', '', $_GET['h']));}
+if (!empty($_GET['id'])){       $Identificador         = str_replace('%20', '', str_replace(' ', '', $_GET['id']));}
+if (!empty($_GET['f'])){        $Fecha                 = str_replace('%20', '', str_replace(' ', '', $_GET['f']));}
+if (!empty($_GET['h'])){        $Hora                  = str_replace('%20', '', str_replace(' ', '', $_GET['h']));}
 if ( isset($_GET['lt'])){       $GeoLatitud            = str_replace('%20', '', str_replace(' ', '', $_GET['lt']));}
 if ( isset($_GET['lg'])){       $GeoLongitud           = str_replace('%20', '', str_replace(' ', '', $_GET['lg']));}
 if ( isset($_GET['v'])){        $GeoVelocidad          = str_replace('%20', '', str_replace(' ', '', $_GET['v']));}
 if ( isset($_GET['d'])){        $GeoDireccion          = str_replace('%20', '', str_replace(' ', '', $_GET['d']));}
+if ( isset($_GET['dl'])){       $Dataloger             = str_replace('%20', '', str_replace(' ', '', $_GET['dl']));}
 if ( isset($_GET['m'])){        $GeoMovimiento         = str_replace('%20', '', str_replace(' ', '', $_GET['m']));}
 if ( isset($_GET['ups'])){      $ups                   = str_replace('%20', '', str_replace(' ', '', $_GET['ups']));}
 //Sensores Telemetria
